9113fd244dcef979ce51231eb9be30648e07393c
Efraim Munthe <efrayanglain@gmail.com>
2023-12-12 15:14:04 +0700

    add browser client

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,22 +1,28 @@
-import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import {
+  createServerClient,
+  createBrowserClient,
+  type CookieOptions,
+} from "@supabase/ssr";
 
 const supabaseURL = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const createClient = (cookies: any) => {
-  return createServerClient(
-    supabaseURL, supabaseAnonKey, {
+  return createServerClient(supabaseURL, supabaseAnonKey, {
     cookies: {
       get(key: string) {
-        return cookies.get(key)?.value
+        return cookies.get(key)?.value;
       },
       set(key: string, value: string, options: CookieOptions) {
-        return cookies.set(key, value, options)
+        return cookies.set(key, value, options);
       },
       remove(key: string, options) {
-        cookies.delete(key, options)
-      }
-    }
-  }
-  )
-}
+        cookies.delete(key, options);
+      },
+    },
+  });
+};
+
+export const createUsualClient = () => {
+  return createBrowserClient(supabaseURL, supabaseAnonKey);
+};
