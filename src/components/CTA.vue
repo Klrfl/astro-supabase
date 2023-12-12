8502c94b25b7fea7b75cd7fefa940b98df7c78b7
Efraim Munthe <efrayanglain@gmail.com>
2023-12-12 15:20:04 +0700

    move CTA component to vue

---
+++ src/components/CTA.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+  isLink: Boolean,
+});
+</script>
+
+<template>
+  <a class="cta" v-if="isLink">
+    <slot />
+  </a>
+  <button class="cta" v-else>
+    <slot />
+  </button>
+</template>
+
+<style>
+.cta {
+  @apply bg-slate-200 px-4 py-2 rounded-md shadow;
+}
+
+.cta:hover {
+  @apply bg-slate-300;
+}
+
+.cta:active {
+  translate: 0 4px;
+}
+
+cta:focus-visible {
+  @apply ring;
+}
+</style>
