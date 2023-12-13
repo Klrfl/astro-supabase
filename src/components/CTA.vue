453364eabda436bbb29bf4dee6f91193bb7e3795
Efraim Munthe <efrayanglain@gmail.com>
2023-12-13 07:35:37 +0700

    change CTA styles

--- src/components/CTA.vue
+++ src/components/CTA.vue
@@ -1,32 +1,49 @@
 <script setup>
 const props = defineProps({
   isLink: Boolean,
+  danger: Boolean,
+  fill: {
+    type: Boolean,
+    default: false,
+  },
 });
 </script>
 
 <template>
-  <a class="cta" v-if="isLink">
+  <a :class="['cta', { fill: fill }, { danger: danger }]" v-if="isLink">
     <slot />
   </a>
-  <button class="cta" v-else>
+  <button :class="['cta', { fill: fill }, { danger: danger }]" v-else>
     <slot />
   </button>
 </template>
 
 <style>
 .cta {
-  @apply bg-slate-200 px-4 py-2 rounded-md shadow;
+  @apply px-4 py-2 rounded-md shadow outline outline-2 outline-slate-200;
 }
 
 .cta:hover {
-  @apply bg-slate-300;
+  @apply bg-slate-200;
 }
 
 .cta:active {
   translate: 0 4px;
 }
 
-cta:focus-visible {
-  @apply ring;
+.fill {
+  @apply bg-slate-200;
+}
+
+.fill:hover {
+  @apply bg-slate-300;
+}
+
+.danger {
+  @apply bg-transparent outline outline-2 outline-red-200;
+}
+
+.danger:hover {
+  @apply bg-red-200;
 }
 </style>
