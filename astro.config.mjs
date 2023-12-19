00b77fc0205eb7f0f5bf85ece9920352dc4aa7fd
Efraim Munthe <efrayanglain@gmail.com>
2023-12-19 16:38:14 +0700

    update netlify adapter (hosting now fixed)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 
-import node from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), vue()],
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify(),
 });
-
